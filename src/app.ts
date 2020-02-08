271ebb7d64fdce468269517ce098e27be826e832
BrooksPoltl <bpoltl1@gmail.com>
2020-02-08 17:58:37 -0600

    db connection and docker config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import dbConfig from './dbConfig';
+import sql from 'mssql';
+
+const pool = new sql.ConnectionPool(dbConfig).connect();
 
 const app = express();
-const port = process.env.port ? process.env.port : 3000;
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello world!');
+});
+
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
