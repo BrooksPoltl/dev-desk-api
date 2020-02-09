7c11980d5cddc2fbb315305635fa9c0fc4c57c69
BrooksPoltl <bpoltl1@gmail.com>
2020-02-09 16:55:02 -0600

    closed tickets

--- src/controllers/ticket.ts
+++ src/controllers/ticket.ts
@@ -49,6 +49,27 @@ export const getOpenTickets: RequestHandler = (req, res, next) => {
   }
 };
 
-export const getClosedTickets: RequestHandler = (req, res, next) => {};
+export const getClosedTickets: RequestHandler = (req, res, next) => {
+  try {
+    db('ticket')
+      .where({ open: false })
+      .then((tickets: Ticket[]) => {
+        res.status(200).json({ status: 200, tickets });
+      })
+      .catch(() => {
+        const errorMessage: ErrorHandler = {
+          status: 400,
+          message: 'could not get tickets'
+        };
+        res.status(400).json(errorMessage);
+      });
+  } catch (e) {
+    const errorMessage: ErrorHandler = {
+      status: 500,
+      message: 'Server error'
+    };
+    res.status(500).json(errorMessage);
+  }
+};
 
 export const getMyTickets: RequestHandler = (req, res, next) => {};
